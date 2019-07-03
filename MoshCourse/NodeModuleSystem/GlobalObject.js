bbb6084576255ce927d62887ddc1fd77058c1761
GatesKennedy <conor@esccollective.com>
2019-07-03 15:59:38 -0700

    Mosh Examples: Node Module System

---
+++ MoshCourse/NodeModuleSystem/GlobalObject.js
@@ -0,0 +1,16 @@
+// global Objects
+console.log();
+
+setTimeout(); 
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+// 'window' object is not in Node
+    // window.setTimeout()
+
+global.console.log();
+global.setInterval();
+
+   
--- MoshCourse/app.js
+++ MoshCourse/app.js
@@ -3,3 +3,20 @@ function sayHello(name) {
 }
 
 sayHello('Conor');
+
+// var logger = require('./NodeModuleSystem/logger');
+
+// BEST PRACTICE
+const logger = require('./NodeModuleSystem/logger');
+console.log(logger);
+
+/*
+CONSOLE RESULT:
+
+C: \Programming\ NodeJS\ NodeLearning\ MoshCourse > node app.js
+Hello Conor {
+    log: [Function: log],
+    endPoint: 'http://mylogger.io/log'
+}
+
+*/
