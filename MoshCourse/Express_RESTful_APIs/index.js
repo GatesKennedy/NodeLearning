625266c127d9aea05a7d708f511c6a4dea8c8fb7
GatesKennedy <conor@esccollective.com>
2019-07-10 14:35:14 -0700

    MOD: index.js / route parameters / query strings / HTTP GET handling

--- MoshCourse/Express_RESTful_APIs/index.js
+++ MoshCourse/Express_RESTful_APIs/index.js
@@ -12,6 +12,13 @@ const app = express();
 // app.put();
 // app.delete();
 
+//  Array of Course Objects
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+]
+
 //  GET method
     //  Two Args| path/url, callbackFxn( request, response)
 app.get('/', (req, res) => {
@@ -19,7 +26,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3,]);
+    res.send(courses);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+// Route Parameters
+    // /api/courses/:<paraValue>
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('There is no such course...'); //404
+    res.send(course);
+});
+
+//  Route Multiple Params
+    //  /api/calendar/:<param1>/:<param2>
+app.get('/api/calendar/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+//  Query String Params
+    //  /api/posts/:<param1>/:<param2>?sortBy=name
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+});
+
+//  PORT (environment variable)
+const port = process.env.PORT || 3000;
+
+//  Register Listener
+app.listen(port, () => console.log(`Listening on port ${port}...`));
